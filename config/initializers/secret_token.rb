06e77478391256747c7deffdb298d47adb4f8d57
Victor Stabile <vmstabile@gmail.com>
2012-03-14 02:49:44 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Politizado::Application.config.secret_token = 'e22b182408322642066416a3d590e459021a44cc5852fff493b108ba9c91f72c6d0a131a430eb91b1407136a801512f7eae60db35f3ad47f13e87930218dc40d'
