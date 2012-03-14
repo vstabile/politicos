6793ddb5a8f5ead9ac8a29b03c2f107d6d894b63
Victor Stabile <vmstabile@gmail.com>
2012-03-14 03:33:16 -0300

    devise

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,9 @@ module Politizado
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Devise/Heroku compatibility
+    config.assets.initialize_on_precompile = false
+
   end
 end
